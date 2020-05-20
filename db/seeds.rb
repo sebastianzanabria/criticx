c61008bb7cc3ebcbabba85142af532c764d55fde
sebas_z <zanabria.s@pucp.pe>
2020-05-19 21:48:23 -0500

    Task 2 finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Resolving tasks...'
 Game.create(name: "Rune Factory 4 Special", genre: "Role-playing (RPG)", price: 5999, release_date: "2019-07-25")
 game = Game.new
 game.name = "Borderlands 3"
 game.genre = "Simulator"
 game.price = 5999
 game.release_date = "2019-09-13"
-
+game.save
 arr_games = [ {:name=>"Zombie Driver: Immortal Edition", :genre=>"Racing", :price=>5999, :release_date=>"2019-Jul-25"},
     {:name=>"Remnant: From the Ashes", :genre=>"Shooter", :price=>6999, :release_date=>"2019-Aug-20"},
     {:name=>"Monster Hunter: World - Iceborne", :genre=>"Adventure", :price=>6999, :release_date=>"2019-Sep-06"},
@@ -20,3 +22,21 @@ arr_games = [ {:name=>"Zombie Driver: Immortal Edition", :genre=>"Racing", :pric
     {:name=>"Children of Morta", :genre=>"Role-playing (RPG)", :price=>6999, :release_date=>"2019-Sep-03"} ]
 
 arr_games.each {|g| Game.create(**g)}
+
+puts "\nTask 2 finished"
+puts "\nAll games:"
+Game.all.each {|h| puts h.to_json}
+puts "\nGame id #4:"
+puts Game.find_by(id: 4).to_json
+puts "\nFirst game:"
+puts Game.first.to_json
+puts "\nLast game:"
+puts Game.last.to_json
+puts "\nGames order by release date:"
+Game.order(:release_date).each {|h| puts h.to_json}
+puts "\nGames that cost $59.99:"
+Game.where(price: 5999).each {|h| puts h.to_json}
+puts "\nGames that were released in 2019"
+Game.where('extract(year from release_date) = ?', 2019).each {|h| puts h.to_json}
+puts "\nTask 2 finished"
+
