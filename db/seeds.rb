0aec49cf7615dedcd77cf39949541209d6672184
sebas_z <zanabria.s@pucp.pe>
2020-05-19 20:57:40 -0500

    Add sample Games

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Game.create(name: "Rune Factory 4 Special", genre: "Role-playing (RPG)", price: 5999, release_date: "2019-07-25")
+game = Game.new
+game.name = "Borderlands 3"
+game.genre = "Simulator"
+game.price = 5999
+game.release_date = "2019-09-13"
+
+arr_games = [ {:name=>"Zombie Driver: Immortal Edition", :genre=>"Racing", :price=>5999, :release_date=>"2019-Jul-25"},
+    {:name=>"Remnant: From the Ashes", :genre=>"Shooter", :price=>6999, :release_date=>"2019-Aug-20"},
+    {:name=>"Monster Hunter: World - Iceborne", :genre=>"Adventure", :price=>6999, :release_date=>"2019-Sep-06"},
+    {:name=>"Cyberpunk 2077", :genre=>"Role-playing (RPG)", :price=>4999, :release_date=>"2020-Sep-17"},
+    {:name=>"Pokémon Shield", :genre=>"Role-playing (RPG)", :price=>5999, :release_date=>"2019-Nov-15"},
+    {:name=>"Children of Morta", :genre=>"Role-playing (RPG)", :price=>6999, :release_date=>"2019-Sep-03"} ]
+
+arr_games.each {|g| Game.create(**g)}
