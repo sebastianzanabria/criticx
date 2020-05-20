845970eacef2087d4be218cfe91da97e5b2122f2
sebas_z <zanabria.s@pucp.pe>
2020-05-19 22:04:00 -0500

    Task 4 finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts 'Resolving tasks...'
+puts "\nResolving tasks..."
 Game.create(name: "Rune Factory 4 Special", genre: "Role-playing (RPG)", price: 5999, release_date: "2019-07-25")
 game = Game.new
 game.name = "Borderlands 3"
@@ -28,6 +28,8 @@ puts "\nAll games:"
 Game.all.each {|h| puts h.to_json}
 puts "\nGame id #4:"
 puts Game.find_by(id: 4).to_json
+puts "\nGame that was released on 2019-Aug-20:"
+puts Game.find_by(release_date: "2019-Aug-20").to_json
 puts "\nFirst game:"
 puts Game.first.to_json
 puts "\nLast game:"
@@ -36,7 +38,22 @@ puts "\nGames order by release date:"
 Game.order(:release_date).each {|h| puts h.to_json}
 puts "\nGames that cost $59.99:"
 Game.where(price: 5999).each {|h| puts h.to_json}
-puts "\nGames that were released in 2019"
+puts "\nGames that were released in 2019:"
 Game.where('extract(year from release_date) = ?', 2019).each {|h| puts h.to_json}
-puts "\nTask 2 finished"
+puts "\nTask 3 finished"
+
+
+game = Game.find_by(name: 'Borderlands 3')
+game.genre = 'Shooter'
+game.save
+
+game = Game.find_by(name: 'Pokémon Shield')
+game.update(price: 3999)
+
+Game.where(genre: "Role-playing (RPG)").update_all "price = 2999"
+
+puts "\nUpdated games:"
+Game.all.each {|h| puts h.to_json}
+
+puts "\nTask 4 finished"
 
